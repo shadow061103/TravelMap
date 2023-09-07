42c16875210897b899d022ee35fe70ebd6d27c26
steve_lin <steve_lin@pxpayplus.com>
2023-09-07 17:42:36 +0800

    add hangfire

--- src/Application/TravelMap.Api/Program.cs
+++ src/Application/TravelMap.Api/Program.cs
@@ -1,11 +1,61 @@
+using NLog.Web;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Hangfire.Console;
+using Hangfire.Dashboard;
+using System.Text.Json.Serialization;
+using TravelMap.Core;
+using TravelMap.Api.Filters;
+
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+var services = builder.Services;
+
+services.AddControllers()
+        .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = new UnderLinePolicy();
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddLogging(config =>
+{
+    // clear out default configuration
+    config.ClearProviders();
+    config.AddConfiguration(configuration.GetSection("Logging"));
+
+    if (env == "Development")
+    {
+        config.AddConsole();
+    }
+});
+
+builder.Host.UseNLog();
+
+//hangfire
+services.AddHangfire(configuration =>
+{
+    var storageOptions = new MemoryStorageOptions()
+    {
+        FetchNextJobTimeout = TimeSpan.FromMinutes(60)
+    };
+
+    configuration.UseMemoryStorage(storageOptions)
+    .UseConsole()
+    .UseDashboardMetric(DashboardMetrics.EnqueuedAndQueueCount)
+    .UseDashboardMetric(DashboardMetrics.ProcessingCount)
+    .UseDashboardMetric(DashboardMetrics.FailedCount)
+    .UseDashboardMetric(DashboardMetrics.SucceededCount);
+});
+
+services.AddHangfireServer();
 
 var app = builder.Build();
 
@@ -16,10 +66,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHangfireDashboard(
+                   pathMatch: "/hangfire",
+                   options: new DashboardOptions()
+                   {
+                       Authorization = new[] { new HangfireAuthorizationFilter() }
+                   }
+               );
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHangfireDashboard();
 
 app.Run();
---
+++ src/Common/TravelMap.Core/UnderLinePolicy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TravelMap.Core
+{
+    public class UnderLinePolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            return UnderLineString(name.ToCharArray());
+        }
+
+        private static string UnderLineString(Span<char> chars)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(char.ToLower(chars[0]));
+            for (int i = 1; i < chars.Length; i++)
+            {
+                if (char.IsUpper(chars[i]))
+                {
+                    stringBuilder.Append("_");
+                }
+
+                stringBuilder.Append(char.ToLower(chars[i]));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
